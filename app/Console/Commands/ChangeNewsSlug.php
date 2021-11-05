d54f011933d03a38a9445ded11998da74db585b1
travka47 <travka.eal@students.dvfu.ru>
2021-11-05 22:15:57 +1000

    Lesson 7: Add redirecting middleware

---
+++ app/Console/Commands/ChangeNewsSlug.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\News;
+use App\Models\Redirect;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ChangeNewsSlug extends Command
+{
+    protected $signature = 'change_news_slug {oldSlug} {newSlug}';
+
+    protected $description = 'Command description';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $oldSlug = $this->argument('oldSlug');
+        $newSlug = $this->argument('newSlug');
+
+        if ($oldSlug === $newSlug)
+        {
+            $this->error('The slugs are equal');
+            return 1;
+        }
+
+        $redirect = Redirect::query()
+            -> where('old_slug', parse_url(route('news_item', ['slug' => $oldSlug], false))['path'])
+            -> where('new_slug', parse_url(route('news_item', ['slug' => $newSlug], false))['path'])
+            ->first();
+        if ($redirect !== null)
+        {
+            $this->error('The same request for redirect had already been made');
+            return 1;
+        }
+
+        $news = News::where('slug', $oldSlug)->first();
+        if ($news === null)
+        {
+            $this->error('The news wasn\'t found');
+            return 1;
+        }
+
+        DB::transaction(function() use ($news, $newSlug) {
+            Redirect::where('old_slug', parse_url(route('news_item', ['slug' => $newSlug], false))['path'])->delete();
+            $news->slug = $newSlug;
+            $news->save();
+        });
+
+        return 0;
+    }
+}
--- app/Models/News.php
+++ app/Models/News.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Carbon\Traits\Timestamp;
+use Carbon\Carbon;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @property string $title
+ * @property string $slug
+ * @property string|null $description
+ * @property string $text
+ * @property bool $is_published
+ * @property Carbon $published_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+*/
 
 class News extends Model
 {
@@ -18,5 +30,20 @@ class News extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public function save(array $options = [])
+    {
+        if ($this->exists && $this->isDirty('slug'))
+        {
+            $oldSlug = $this->getOriginal('slug');
+            $newSlug = $this->slug;
+
+            $redirect = new Redirect();
+            $redirect->old_slug = parse_url(route('news_item', ['slug' => $oldSlug], false))['path'];
+            $redirect->new_slug = parse_url(route('news_item', ['slug' => $newSlug], false))['path'];
+            $redirect->save();
+        }
+        return parent::save($options);
     }
 }
