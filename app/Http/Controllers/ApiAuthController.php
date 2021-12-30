b2ff3c1e8db49473119908d7d503c5d33c54c063
travka47 <travka.eal@students.dvfu.ru>
2021-12-30 14:53:46 +1000

    Lesson 10: Add web and API auth and profile

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -15,7 +15,7 @@ class ApiAuthController extends Controller
     public function registration(Request $request) : JsonResponse {
         $request['login'] = strtolower($request['login']);
         $validator = Validator::make($request->all(), [
-            'login' => 'unique:users|required|between:5, 30|regex: /^[\w\-]+$/',
+            'login' => 'unique:users|required|between:5, 30|regex: /^[a-z\.-]+$/i',
             'password' => 'required|between:10, 30|regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&].{10,}$/'
         ]);
 
